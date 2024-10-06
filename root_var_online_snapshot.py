6676c31e24d296a88ff5c1518bd0f7bee2f100ee
omkar <iamok1117@gmail.com>
2024-10-06 10:45:02 +0530

    first_version

---
+++ root_var_online_snapshot.py
@@ -0,0 +1,51 @@
+import boto3
+client = boto3.client('ec2')
+response = client.describe_instance_attribute(
+    Attribute='blockDeviceMapping',
+    InstanceId='i-bjdnbdjmw'
+)
+my_snapshot_volume_list=[]
+if 'BlockDeviceMappings' in response:
+    my_device_list = response['BlockDeviceMappings']
+    
+
+    for i in my_device_list:
+        if i['DeviceName'] == 'sda1':
+            my_volume = i['Ebs']['VolumeId']
+            my_snapshot_volume_list.append(my_volume)
+        if i['DeviceName'] == 'sdf':
+            my_volume = i['Ebs']['VolumeId']
+            my_snapshot_volume_list.append(my_volume)
+if len(my_snapshot_volume_list)<2:
+    print(f"root and var volume are not listed correctly")
+else:
+    print("root volume is {my_snapshot_volume_list[0]}"),
+    print("var volume is {my_snapshot_volume_list[1]}")
+resp_root = client.create_snapshot(
+    VolumeId = my_snapshot_volume_list[0]
+)
+resp_var = client.create_snapshot(
+    VolumeId = my_snapshot_volume_list[1]
+)
+root_snapshot_id = resp_root['SnapshotId'],
+var_snapshot_id = resp_var['SnapshotId']
+
+#print the snapshot details along with volume
+
+print(f"root snapshot is triggered {root_snapshot_id} for the volume{my_snapshot_volume_list[0]}")
+print(f"root snapshot is triggered {var_snapshot_id} for the volume{my_snapshot_volume_list[1]}")
+
+waiter = client.get_waiter('snapshot_completed')
+print("Waiting for root snapshot to complete...")
+waiter.wait(
+    SnapshotIds = [root_snapshot_id]
+)
+print(f"root snapshot {root_snapshot_id} is completed for volume {my_snapshot_volume_list[0]}")
+
+print("waiting for var snapshot to complete ..")
+waiter.wait(
+    SnapshotIds = [var_snapshot_id]
+)
+print(f"var snapshot {var_snapshot_id} is completed for the volume {my_snapshot_volume_list[1]}")
+
+
